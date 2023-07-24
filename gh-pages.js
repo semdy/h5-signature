41242dc770ce91a4f207bda7bd5b32278a66f443
semdy <501859275@qq.com>
2023-07-24 19:01:47 +0800

    update gh-pages.js

--- gh-pages.js
+++ gh-pages.js
@@ -1,17 +1,17 @@
 const fs = require("fs")
 const path = require('path')
-const ghPagesDir = path.resolve(__dirname, './gh-pages')
+const demoDir = path.resolve(__dirname, './demo')
 const htmlPath = path.resolve(__dirname, './index.html')
 const libjsPath = path.resolve(__dirname, './lib/index.js')
 
 function main() {
   try {
-    if (fs.existsSync(ghPagesDir)) {
-      fs.rmSync(ghPagesDir, { recursive: true, force: true })
+    if (fs.existsSync(demoDir)) {
+      fs.rmSync(demoDir, { recursive: true, force: true })
     }
-    fs.mkdirSync(ghPagesDir)
-    fs.copyFileSync(htmlPath, path.resolve(ghPagesDir, './index.html'))
-    fs.copyFileSync(libjsPath, path.resolve(ghPagesDir, './index.js'))
+    fs.mkdirSync(demoDir)
+    fs.copyFileSync(htmlPath, path.resolve(demoDir, './index.html'))
+    fs.copyFileSync(libjsPath, path.resolve(demoDir, './index.js'))
   } catch(e) {
     console.log(e)
     process.exit(1)
