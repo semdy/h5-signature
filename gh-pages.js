e44f0017450be15cfdbf0f963a8f76225e1c15f7
semdy <501859275@qq.com>
2023-07-24 19:07:51 +0800

    update gh-pages.js

--- gh-pages.js
+++ gh-pages.js
@@ -2,7 +2,7 @@ const fs = require("fs")
 const path = require('path')
 const demoDir = path.resolve(__dirname, './demo')
 const htmlPath = path.resolve(__dirname, './index.html')
-const libjsPath = path.resolve(__dirname, './lib/index.js')
+const libjsPath = path.resolve(__dirname, './lib/index.min.js')
 
 function main() {
   try {
@@ -11,7 +11,7 @@ function main() {
     }
     fs.mkdirSync(demoDir)
     fs.copyFileSync(htmlPath, path.resolve(demoDir, './index.html'))
-    fs.copyFileSync(libjsPath, path.resolve(demoDir, './index.js'))
+    fs.copyFileSync(libjsPath, path.resolve(demoDir, './index.min.js'))
   } catch(e) {
     console.log(e)
     process.exit(1)
